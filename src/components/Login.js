fd0dcb14d49025c8dba9014d1617f37d047955c8
Shalinishalu23 <Shalu.shalinigowda@gmail.com>
2024-07-10 16:41:18 +0530

    Shalini--Removed css

--- src/components/Login.js
+++ src/components/Login.js
@@ -69,7 +69,7 @@ const Login = () => {
                 {/* <img
                     src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background"
                 />    */}
-                <img className="h-screen object-cover" src={BG_URL} alt="logo" />
+                <img src={BG_URL} alt="logo" />
             </div>
             <form onSubmit={(e) => e.preventDefault()} className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
                 <h1 className="font-bold text-3xl py-4">
