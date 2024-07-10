74264eff006b6a667c191e2490eae1b38c3a3c07
Shalinishalu23 <Shalu.shalinigowda@gmail.com>
2024-07-10 16:20:50 +0530

    Shalini--Responsive design

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -43,8 +43,8 @@ const GptSearchBar = () => {
               );
     }
     return (
-        <div className='pt-[20%] flex justify-center'>
-            <form className='w-1/2 bg-black grid grid-cols-9' onSubmit={(e)=>e.preventDefault()}>
+        <div className='pt-[35%] md:pt-[10%] flex justify-center'>
+            <form className='w-full md:w-1/2 bg-black grid grid-cols-9' onSubmit={(e)=>e.preventDefault()}>
                 <input ref={searchText} type='text' className='p-4 m-4 col-span-6' placeholder={lang[langKey].gptSearchPlaceholder} />
                 <button className='col-span-3 py-2 px-4 bg-red-700 m-4 text-white rounded-lg' onClick={handleGptSearchClick }>{lang[langKey].search}</button>
             </form>
